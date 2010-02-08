0756aaa7b8432d45a2c8dd849a465b534414a028
designerbrent <brent@positivechoices.com>
2010-02-08 14:48:15 -0800

    Adjusting vertical layouts.

--- sociallinks-block.tpl.php
+++ sociallinks-block.tpl.php
@@ -1,6 +1,7 @@
 <div id="sociallinks">
-  <ul class="sociallinks-tabs">
+  <ul class="sociallinks-tabs clear-block">
     <?php print drupal_render($tabs); ?>
   </ul>
   <?php print drupal_render($content); ?>
+  <div class="clear-block"></div>
 </div>
