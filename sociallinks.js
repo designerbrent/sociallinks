0a170b30677359a06987dcf1d36c797aff549410
Brent Hardinge <designerbrent@gmail.com>
2010-10-27 16:19:52 -0700

    This is mostly functional, however we need to redo the JS.

--- sociallinks.js
+++ sociallinks.js
@@ -1,8 +1,8 @@
 // $Id: sociallinks.js,v 1.1 2010/02/10 23:43:47 greenskin Exp $
 
 Drupal.behaviors.socialLinks = function(){
-  $("#sociallinks.button_click").tabs();
-  $("#sociallinks.button_hover").tabs({
-    event: 'mouseover'
-    });
+  // $("#sociallinks.button_click").tabs();
+  // $("#sociallinks.button_hover").tabs({
+  //   event: 'mouseover'
+  //   });
 }
