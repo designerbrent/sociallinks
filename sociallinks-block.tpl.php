6681e4b9b193a820ca2157afb677cd951deb30d0
James Jeffery <james@positivechoices.com>
2010-10-27 17:08:15 -0700

    Renamed 'button_action' to 'tab_event'.

--- sociallinks-block.tpl.php
+++ sociallinks-block.tpl.php
@@ -1,4 +1,4 @@
-<div id="sociallinks" class="<?php print $button_action ?>">
+<div id="sociallinks" class="<?php print $tab_event ?>">
   <ul class="sociallinks-tabs clear-block">
     <?php print drupal_render($tabs); ?>
   </ul>
