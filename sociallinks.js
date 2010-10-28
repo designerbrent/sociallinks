0c2a07a48a9eb8c870972ab0f0f8effe12ca43a1
James Jeffery <james@positivechoices.com>
2010-10-27 22:41:25 -0700

    Prepping for new hover and click event implementations.

--- sociallinks.js
+++ sociallinks.js
@@ -1,8 +1,21 @@
 // $Id: sociallinks.js,v 1.1 2010/02/10 23:43:47 greenskin Exp $
 
-Drupal.behaviors.socialLinks = function(){
-  // $("#sociallinks.button_click").tabs();
-  // $("#sociallinks.button_hover").tabs({
-  //   event: 'mouseover'
-  //   });
+Drupal.behaviors.sociallinks = function(){
+  Drupal.settings.sociallinks.tabEvent;
+  $('#sociallinks li.tab').each(function(i) {
+    switch (Drupal.settings.sociallinks.tabEvent) {
+      // Hover
+      case 1:
+        $(this).hover(function() {
+          
+        });
+        break;
+      // Click
+      case 2:
+        $(this).click(function() {
+          
+        });
+        break;
+    }
+  });
 }
